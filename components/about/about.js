609b695c2c32f570d6b8c10a23922fcdfecff9c5
Bart Nowak <bnowak@bnowak.com>
2020-08-04 15:33:01 +0200

    Maintenance

--- components/about/about.js
+++ components/about/about.js
@@ -1,5 +1,5 @@
 import cx from 'classnames';
-import { forwardRef, useImperativeHandle } from 'react';
+import { forwardRef, useImperativeHandle, useRef, createRef } from 'react';
 import { useEffect, useState } from 'react';
 import { IconArrowLeft } from './../icons/icons';
 
@@ -10,27 +10,50 @@ import {
   blockReveal,
   revealOpen,
   revealClosed,
+  postTransitionReveal,
   backIcon,
   backLabel,
   backButton,
 } from './about.scss';
 
 const About = forwardRef((props, ref) => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, _setIsVisible] = useState(false);
+  const [isInTransition, setIsInTransition] = useState(false);
   const getIsVisible = () => isVisible;
+  const setIsVisible = (value) => {
+    _setIsVisible(value);
+    setIsInTransition(true);
+  };
   useImperativeHandle(ref, () => ({
     getIsVisible,
     setIsVisible,
   }));
   const hide = () => setIsVisible(false);
+  const sectionRef = createRef();
+  const onTransitionEnd = () => {
+    setIsInTransition(false)
+  };
+  useEffect(() => {
+    const s = sectionRef.current;
+    s.addEventListener('transitionend', onTransitionEnd);
+
+    return () => {
+      s.removeEventListener('transitionend', onTransitionEnd);
+    };
+  }, [sectionRef]);
   return (
     <div
       ref={ref}
       {...props}
-      className={cx(blockReveal, isVisible ? revealOpen : revealClosed)}
+      className={cx(
+        blockReveal,
+        isVisible ? revealOpen : revealClosed,
+        isInTransition ? '' : postTransitionReveal
+      )}
     >
       <section
         className={cx(aboutSection, isVisible ? sectionOpen : sectionClosed)}
+        ref={sectionRef}
       >
         <button className={backButton} onClick={hide} aria-hidden="true">
           <IconArrowLeft className={backIcon} />
@@ -38,17 +61,24 @@ const About = forwardRef((props, ref) => {
         </button>
         <p>Dear reader,</p>
         <p>
-          My name is Bart Nowak. I was born in Poland in 1983 and got
-          commissioned to build a first digital project around the year 2000.
+          I have started my adventure as a graphic designer and got commissioned
+          to build my first digital, web project in the year 2000.
         </p>
         <p>
-          In the past I've had the privilege of working with Nike, HSBC, JP
-          Morgan and Sapient Global Markets amongst others.
+          Since then I have gained experience in market areas such as Finance,
+          Healthcare, eCommerce/Retail, including the privilege of working with
+          Nike, HSBC, JP Morgan, and Sapient Global Markets amongst others.
+        </p>
+        <p>
+          Today my main focus areas are end-to-end web architecture, efficient
+          CI/CD processes, mentoring developers, and integration within a wider,
+          corporate context.
         </p>
         <p>
           I am driven by the need to make a difference. I want to help other
-          people by creating a better working environment, while building
-          systems and solutions that make a positive impact.
+          people succeed by empowering them to deliver at the top of their
+          performance, while building systems and solutions that make a positive
+          impact.
         </p>
         <p>Thank you for stopping by.</p>
       </section>
